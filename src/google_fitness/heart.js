bc2e42423d30380d5baeaba0ec518a0fb23ba698
Rodrigo Maafs <rodrigoelmaps@gmail.com>
2024-11-19 01:57:09 -0600

    hotfix: Use right nanoseconds

--- src/google_fitness/heart.js
+++ src/google_fitness/heart.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const url = require("url");
+const BigInt = require("big-integer");
 
 const GOOGLE_REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
@@ -90,8 +91,18 @@ export default class Heart {
   async sendFitnessRequest(timeLapsed) {
     const dataSource =
       "derived:com.google.heart_rate.bpm:com.google.android.gms:merge_heart_rate_bpm";
-    const startDate = Date.now() - timeLapsed + "000000";
-    const endDate = Date.now() + "000000";
+
+    const startDate = getCurrentNanoseconds(timeLapsed);
+    const endDate = getCurrentNanoseconds();
+
+    console.log(
+      "https://www.googleapis.com/fitness/v1/users/me/dataSources/" +
+        dataSource +
+        "/datasets/" +
+        endDate +
+        "-" +
+        startDate
+    );
 
     return new Promise((resolve, reject) => {
       axios({
@@ -119,6 +130,7 @@ export default class Heart {
             const response = {
               value: rate.value[0].fpVal,
               time: rate.endTimeNanos,
+              modifiedTimeMillis: rate.modifiedTimeMillis,
             };
             resolve(response);
           } else {
@@ -130,4 +142,15 @@ export default class Heart {
         });
     });
   }
+}
+
+function getCurrentNanoseconds(removeTime = 0) {
+  const loadNs = process.hrtime();
+  const loadMs = new Date().getTime() - removeTime;
+
+  let diffNs = process.hrtime(loadNs);
+  return BigInt(loadMs)
+    .times(1e6)
+    .add(BigInt(diffNs[0]).times(1e9).plus(diffNs[1]))
+    .toString();
 }
